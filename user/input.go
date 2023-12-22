0c894434531860632fa8f4ed679816e612c93778
Rifaldi Yoga <rifaldi.yoga@bee.id>
2023-12-22 11:07:24 +0700

    revisi

--- user/input.go
+++ user/input.go
@@ -28,7 +28,7 @@ type CreateUserInput struct {
 	Name       string `json:"name" binding:"required"`
 	Email      string `json:"email" binding:"required,email"`
 	Password   string `json:"password"`
-	Role       string `json:"role" binding:"required"`
+	Role       string `json:"role"`
 	AvatarPath string `json:"avatar_path"`
 	Token      string `json:"token"`
 }
